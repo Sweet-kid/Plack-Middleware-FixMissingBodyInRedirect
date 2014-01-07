d0c9b48dcf2aa9c2cd0d4e7fae9a31f289e1335b
Upasana <me@upasana.me>
2014-01-08 01:02:11 +0530

    Wrote sub _is_body_set

--- lib/Plack/Middleware/FixMissingBodyInRedirect.pm
+++ lib/Plack/Middleware/FixMissingBodyInRedirect.pm
@@ -33,10 +33,44 @@ sub call {
 EOF
                 $response->[2] = [$body]; # body should be either an array ref or file handle
                 $headers->set('Location' => $encoded_location);
+                $headers->set('Content-Type' => 'text/html; charset=utf-8');
                 return $response;
-	    }
-	}
+            }
+        }
     });
+}
+
+sub _is_body_set {
+    my $response_ref = shift;
+    my @response = @$response_ref;
+    if( scalar( @response ) == 3 ) {
+        my $body_ref = $response[2];
+        my $body_ref_type = ref( $body_ref );
+        if( $body_ref_type eq "ARRAY" ) {
+            my @body = @$body_ref;
+            if( scalar( @body ) == 0 ) {
+                # if size of the body array is 0, then it's not set, so return false
+                return 0;
+            } else {
+                foreach my $element ( @body ) {
+                    if( $element ) {
+                        # if even a single $element is set, then body is set, so return true
+                        return 1;
+                    }
+                }
+                # flow will reach this statement only after traversing
+                # the whole body array in above foreach loop, which means that
+                # no element is set in the body array, so return false
+                return 0;
+            }
+        } elsif( $body_ref_type eq "GLOB" ) {
+            if( -z $body_ref ) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
 }
 
 1;
